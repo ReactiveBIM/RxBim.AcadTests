17c0ff4b47c386aab7bc1be826129162409d11ff
ivachevev <ivachevev@pik.ru>
2023-08-28 12:08:10 +0300

    PIKT-70 dev003

--- build/Build.cs
+++ build/Build.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -7,6 +8,8 @@ using AcadTests.Nuke;
 using Bimlab.Nuke.Components;
 using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using RxBim.Nuke.AutoCAD;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -57,25 +60,36 @@ public class Build : AutocadRxBimBuild, IPublish
                     var assemblyName = project.Name + ".dll";
                     var assemblyPath = outputDirectory / assemblyName;
                     var results = outputDirectory / "result.xml";
-                    var consoleDllPath = typeof(AcadTests.Console.Services.AcadTestTasks).Assembly.Location;
-                    DotNet(consoleDllPath + " " + $@"-a {assemblyPath} -r {results} -v 2019");
+                    ProcessTasks
+                        .StartProcess("revittests.console", $@"-a {assemblyPath} -r {results} -v 2019")
+                        .WaitForExit();
                     var resultPath = outputDirectory / "result.html";
                     await new ResultConverter()
                         .Convert(results, resultPath);
                 }
             });
 
+    Target UpdateRevitTestConsole => _ => _.Executes(() =>
+        DotNetToolUpdate(settings => settings
+            .SetPackageName("RxBim.RevitTests.Console")
+            .SetVersion("1.0.1-dev002")
+            .EnableGlobal()));
+
     Target RevitIntegrationTests =>
         _ => _
             .Executes(async () =>
             {
                 var solution = Solution;
-                var projects = solution.AllProjects
+                List<Project> projects;
+                projects = solution.AllProjects
                     .Where(x => x.Name.EndsWith("Revit.IntegrationTests"))
                     .ToList();
                 if (!projects.Any())
                     throw new ArgumentException("project not found");
-                var consoleDllPath = typeof(RevitTests.Console.Services.RevitTestTasks).Assembly.Location;
+                DotNetToolUpdate(settings => settings
+                    .SetPackageName("RxBim.RevitTests.Console")
+                    .SetVersion("1.0.1-dev002")
+                    .EnableGlobal());
                 foreach (var project in projects)
                 {
                     var outputDirectory = solution.Directory / "testoutput" / project.Name;
@@ -88,7 +102,9 @@ public class Build : AutocadRxBimBuild, IPublish
                     var assemblyName = project.Name + ".dll";
                     var assemblyPath = outputDirectory / assemblyName;
                     var results = outputDirectory / "result.xml";
-                    DotNet(consoleDllPath + " " + $@"-a {assemblyPath} -r {results} -v 2019");
+                    ProcessTasks
+                        .StartProcess("RxBim.RevitTests.Console", $@"-a {assemblyPath} -r {results} -v 2019")
+                        .WaitForExit();
                     var resultPath = outputDirectory / "result.html";
                     await new ResultConverter()
                         .Convert(results, resultPath);
